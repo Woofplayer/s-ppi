55cc41d01655103d24a175fdaedb3098919ba88b
vaksis <54946934+asecoder3@users.noreply.github.com>
2021-09-28 19:27:58 +0300

    styles ja weathercard ja div-hommia

--- src/Weather.js
+++ src/Weather.js
@@ -2,15 +2,24 @@ import React, { useState, useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import { getForecast } from './request'
 import { formatTime } from './utils/formatTime'
+import { render } from '@testing-library/react'
+import WeatherCard from './WeatherCard'
 
-const useStyles = makeStyles(() ({
-    
+const useStyles = makeStyles(() => ({
+    center: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    biggerFont: {
+        fontSize: '5vw'
+    }
 }))
 
 const Weather = () => {
     const [forecasts, setForecast] = useState([])
     const [focusedForecastDT, setFocusedForecastDT] = useState()
-
+    const classes = useStyles()
     useEffect(() => {
         getForecast().then((response) => {
             setForecast(response);
@@ -23,16 +32,29 @@ const Weather = () => {
 
     const selectedForecast = forecasts.list.find(f => f.dt === focusedForecastDT)
 
+    const renderWeatherItem = (forecast) => {
+        return (
+            <div>
+                <WeatherCard forecast={forecast} />
+            </div>
+        )
+    }
+
     return (
         <div>
             <div>
-                {forecasts.city.name}
+                <div className={`${classes.center} ${classes.biggerFont}`}> 
+                    {forecasts.city.name}
+                </div>
+                <div className={classes.center}>
+                    Aurinko nousee klo: {formatTime(forecasts.city.sunrise)}
+                </div>
+                <div className={classes.center}>
+                    Aurinko laskee klo: {formatTime(forecasts.city.sunset)}
+                </div>
             </div>
             <div>
-                Aurinko nousee klo: {formatTime(forecasts.city.sunrise)}
-            </div>
-            <div>
-                Aurinko laskee klo: {formatTime(forecasts.city.sunset)}
+                    {forecasts.list.map(forecast => renderWeatherItem(forecast))}
             </div>
         </div>
     )
